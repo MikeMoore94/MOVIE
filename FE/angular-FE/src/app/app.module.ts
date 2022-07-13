e029efda71b174e0faacf3019af569213f5f2fd1
Micheal Moore <mmoore@xpanxion.com>
2022-07-13 15:58:26 -0400

    added all comonents

--- FE/angular-FE/src/app/app.module.ts
+++ FE/angular-FE/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateMovieComponent } from './create-movie/create-movie.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { UpdateMovieComponent } from './update-movie/update-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateMovieComponent,
+    MovieDetailsComponent,
+    MovieListComponent,
+    UpdateMovieComponent
   ],
   imports: [
     BrowserModule,
